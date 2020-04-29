abb2639dd3f95b67567a3603c352f8ae54512ee1
Charmant101 <daylayomoarukhe@yahoo.com>
2020-04-29 19:19:31 +0100

    added frontend,coreand server files

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log(`this is my index`);
+const http = require("http");
+
+const fs = require("fs");
+
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+
+const PORT = 3000;
+// const fetchView = (name) => {
+//   return path.resolve(__dirname, "./views/", name);
+// };
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  // how to render static html files
+  //    res.sendFile(fetchView('index.html'));
+
+  //    how to render ejs dynamic files
+  res.render("index",{title:'my todo-application'});
+});
+
+app.get("/login", (req, res, next) => {
+  res.send("This is the login page");
+});
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
+
+// how to use the http core module to set up a server
+
+// const server = http.createServer((req,res)=> {
+//     res.write('hello world');
+//     res.end();
+// })
+
+// server.listen(PORT,()=> {
+//     console.log(`Listening on Port 3000`);
+// });
